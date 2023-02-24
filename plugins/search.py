7ec0318d4871c0a4083032561d927d9624865b54
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-24 10:27:47 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -85,11 +85,9 @@ async def request(bot, update):
 
     admin = (await get_group(update.message.chat.id))["user_id"]
     id    = update.data.split("_")[1]
-    group = await get_group(id)
-    link  = group["name"]
     name  = await search_imdb(id)
     url   = "https://www.imdb.com/title/tt"+id
-    text  = f"#Request\n\n Group Name - {link}\nName: {name}\nIMDb: {url}"
+    text  = f"#Request\n\nName: {name}\nIMDb: {url}"
     await bot.send_message(chat_id=admin, text=text, disable_web_page_preview=True)
     await update.answer("✅ Request Sent To Admin", show_alert=True)
     await update.message.delete(60)
