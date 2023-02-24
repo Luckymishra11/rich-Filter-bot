935e6b63d6bb289f16ad4f1d06601f6462fc0e56
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-24 13:42:09 +0530

    Update verify.py

--- plugins/verify.py
+++ plugins/verify.py
@@ -67,7 +67,7 @@ async def verify_yes(bot, update):
                            disable_web_page_preview=True,
                            reply_markup=InlineKeyboardMarkup(
                            [[InlineKeyboardButton("✅ Approve", callback_data=f"verify_approve_{message.chat.id}"),
-                             InlineKeyboardButton("❌ Decline", callback_data=f"verify_decline_{message.chat.id}")],
+                                                   InlineKeyboardButton("❌ Decline", callback_data=f"verify_decline_{message.chat.id}")],
                             [InlineKeyboardButton("👀 View Group", url=f"{link}")]])) 
     await message.reply("Verification Request sent ✅\nWe will notify You Personally when it is approved")
 
