c58a9a55dde4c029793c6ebbfc9f53a58044c64e
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-24 10:30:24 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -87,7 +87,7 @@ async def request(bot, update):
     id    = update.data.split("_")[1]
     name  = await search_imdb(id)
     url   = "https://www.imdb.com/title/tt"+id
-    text  = f"#Request\n\nName: {name}\nIMDb: {url}"
+    text  = f"#Request\n\nName: `{name}`\nIMDb: {url}"
     await bot.send_message(chat_id=admin, text=text, disable_web_page_preview=True)
     await update.answer("✅ Request Sent To Admin", show_alert=True)
     await update.message.delete(60)
