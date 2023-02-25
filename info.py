c0e9f27283bc51a3abe3db98a56775262de778b3
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-25 06:45:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 12124605
 API_HASH     = "5cf3577d85fd02286535ec2296934287"
-BOT_TOKEN    = "5819892218:AAG5RALzpcyv3dJrQp9RztzVLDz6Gfqb3e0"
+BOT_TOKEN    = "5869198116:AAEaZOzKaifdVizi8_cYz4G38sHmZvkFtJU"
 SESSION      = "BQDw418AVxxqadL7ozy53Pl3ZJ8J30yhPii4rj-R6Yq4iXWwM68Bz3kfeYshTrhAODB1nDE420L7ictw-DmVL7VN70gqxZurqRloCthbJUzu0Tjf2HmM-21sUYoKnsz52gAqT3QDHRcL_wqoF1ygOBUpO7xOthMCnJUTYIPdbiyozfVa80XzUnJV1XwxJpJfMvK0handnrvVOWgPlIT76ZJHPsL1G5rFgKSJ8KHQ6abxA9C0lVulhLG5v47bOMOouV4NJYxsE03iQwOG-8modBqHG8tUfE2Aew7-XUou07WBpRth4RjjQuqx5pN5mlB28CYdqH7KcH6p9g82mF9n54h-IGbznQAAAABwcLzMAA"
 DATABASE_URI = "mongodb+srv://autochannelfilter:autochannelfilter@cluster0.mcpsuva.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001803930395
+LOG_CHANNEL  = -1001868638041
 ADMIN        = 1883570185
 CHANNEL      = "@cynitebackup"
