93475ec111fe4458f93d103178e5f69721cd1924
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-25 10:40:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,4 +7,4 @@ LOG_CHANNEL  = -1001868638041
 ADMIN        = 1883570185
 CHANNEL      = "@cynitebackup"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
-APPROVED     = "on"
+APPROVE     = "on"
