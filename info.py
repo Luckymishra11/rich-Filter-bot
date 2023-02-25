f28ba6653947267d49b3bbc0af2cd15129502cd2
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-02-25 10:32:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,3 +6,5 @@ DATABASE_URI = "mongodb+srv://autochannelfilter:autochannelfilter@cluster0.mcpsu
 LOG_CHANNEL  = -1001868638041
 ADMIN        = 1883570185
 CHANNEL      = "@cynitebackup"
+APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
+APPROVED     = "on"
