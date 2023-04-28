b420b1a3b89468e24ebd4b38432573ef107f44a5
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-04-28 15:46:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 API_ID       = 12124605
 API_HASH     = "5cf3577d85fd02286535ec2296934287"
-BOT_TOKEN    = "5869198116:AAEaZOzKaifdVizi8_cYz4G38sHmZvkFtJU"
-SESSION      = "BQDw418AMczZzW2EXyYjMiwdvLGaJISk8WoykAAPJj7VE7UBGlcz4gVtqPgB427dqjS4DMH0FFzzV5OLLCSija7Gv_NmZsrmvxUecsI11O16At-18xm_gTQ6QGs4KwbITn88riJxZHH6Oyl5aCsetyGI9P3swZQvfMsmzS_jWD6ya3PIaARkxmzCiAS5pC8fysWdOcW96hCIpCDDJsW3vAw3rMKdhuFL71PLg73SIUYVUUiRUzeNX5LoxK4_FCxZveYYTYAwNqoFXQ25yeiaeqiO124IjIedPPzfc3nfVSALMfUd6P324dmCiT92CsnweO34mz9kQJcbHNcAg4p3VMJ782NXZwAAAABwcLzMAA"
-DATABASE_URI = "mongodb+srv://autochannelfilter:autochannelfilter@cluster0.mcpsuva.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001868638041
-ADMIN        = 1883570185
+BOT_TOKEN    = "6262376276:AAHazSHPyVvCvGfpnZ2wDYRoYJp8F4O0yhY"
+SESSION      = "BQFDuXUAci2-0V4YR78BWc9cWqwuudxGChVG6_nNAZGFPNd7LQvpEb54tOjhYrjCxn1KBYqfBDOhlWxGGdB5nBA2s3e6uRyTzOlC4r380efFvz_XzLTUP0DGYa6SDgK82d0_GQ9k5wNWAXUKstfiPcVDUHuqa9YgwTZb5gHnA7iqXOydrXDDYP1wdbaU2FDjhHHwoZ7qYUNj9HNA-KAHo7BEAylumigUhL-RTDHGe4h2jzrXcrQhW_jKTxsDQdBgj5NMLpy7yveB0y1Vf19Ou6cnjqBfsIWQHE4cj6VrncxhaS32N8t1uY-9Fahb3UcYnI_Mkj90U5XLtklpbDqbTeVnM8_nRQAAAAE5qYYzAA"
+DATABASE_URI = "mongodb+srv://SearchBot:SearchBot@cluster0.umbvify.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001697367157
+ADMIN        = 5558463511
 CHANNEL      = "@cynitebackup"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
