2650943d07bbc67646e734a71a008fa45c0098f5
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-05-26 12:41:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ BOT_TOKEN    = "6262376276:AAHazSHPyVvCvGfpnZ2wDYRoYJp8F4O0yhY"
 SESSION      = "BQFDuXUAIN8uwKVV67hl3t94d2C9YPpSdPfLo7wuEIR-IVvbKI1X3jSwCCsQnselcNijs4ix0lP_BUbVUrUkR7XaCpExRLhD8slJNe1VH4ajO11PZTruBnW2N0fr8k5bA3K5lKNc_bokCEVSYSHenPsDuE26SHZTM1MaldQKMUEAFJl59DjFHFEHGD9ce6IxGvQyE6EmB_l1667nliQXsmEQeLmbIM4OEF73todn-leGw_5_m_MW7h7cQBqMuLNt4WQFk423KYLDebRbsR29Xbpt5F57otJjIXcQZEXV7OIQF6NlWIaPGUHSPSnH6K5wiMBUsCG6GF8Zfoi3h6cvmMNvwx_YeAAAAAE5qYYzAA"
 DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001697367157
-ADMIN        = 5558463511
+ADMIN        = 1967654509
 CHANNEL      = "@cynitebackup"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
