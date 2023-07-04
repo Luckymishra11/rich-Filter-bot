765cf5d40537cde9b48e3c2fb45c125959309dc9
Luckymishra11 <126936878+Luckymishra11@users.noreply.github.com>
2023-07-04 20:54:13 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -4,8 +4,9 @@ class script(object):
 I am **Rich Filter bot**
 I Will filter your channel posts automatically and send it in your group chat when Someone search it.
 
-Send /help For More"""
-
+Send /help For More info!"""
+    Send /buy to purchase subscription!"""
+    
     HELP = """To Use me In A Group
 First Buy Subscription With - /buy and Then
 It Will Cost index 50₹ Per Month
