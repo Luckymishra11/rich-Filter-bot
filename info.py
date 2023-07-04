1b7cfa51c798ccb4e66073d059e13f5db9a32878
Luckymishra11 <126936878+Luckymishra11@users.noreply.github.com>
2023-07-04 14:42:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ API_ID       = 12124605
 API_HASH     = "5cf3577d85fd02286535ec2296934287"
 BOT_TOKEN    = "6262376276:AAHazSHPyVvCvGfpnZ2wDYRoYJp8F4O0yhY"
 SESSION      = "BQFDuXUAeSVqHf8mdLT3bTxcT1ZIoh21oTc29Yzd3qSZrDkkD5E3-O3x678bcwThZZrSWd37ij3qtlDvXszBAkFC7ao_s3IsCbpp-8PZDsze_X4MML2qCnLF5Dv-8ZATPVRl8MBUt5jGpUeB7Hah-TfHiY5R0kBKxDM9MXsEglAIrRyjq3i6_rMF_sHRpJfF0NyhdI1rHmeEKT76dwkqti8QYbnqeF2gm1RVB9qq_qDpkQCQgER1fDKwmpE8rxUwGnVnYXMXrP52QMqiyyK1oeLpL-ppaiIdaYkrwq1ow70WNrNGwitICR7eLdz8RSIb8RuFTSeI-Hu9euQcFXhdM_ahZkeAdgAAAAE5qYYzAA"
-DATABASE_URI = "mongodb+srv://mishrajaynishra:f3ewLPvuPeq0M1sB@cluster0.raknipt.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Temp:Temp@cluster0.5zwnkvi.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001697367157
 ADMIN        = 5558463511
 CHANNEL      = "@cynitebackup"
